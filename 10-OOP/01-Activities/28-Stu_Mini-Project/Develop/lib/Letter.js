1a323b350ff7e884ceb13f31cf604c2d44c02fbe
Chadtao206 <chenchen408@gmail.com>
2021-11-20 13:56:02 -0800

    livecode solution

--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,24 @@
 class Letter {
-  
+  constructor(letter) {
+    //do some kind of check to see if is letter or num and set visible prop
+    this.visible = /[^a-z0-9]/gi.test(letter);
+    this.char = letter;
+  }
+  guess(l) {
+    //check to see if l = the letter
+    this.visible = this.visible
+      ? this.visible
+      : l.toLowerCase() === this.char.toLowerCase();
+    return l.toLowerCase() === this.char.toLowerCase();
+  }
+
+  toString() {
+    return this.visible ? this.char : "_";
+  }
+
+  getSolution() {
+    return this.char;
+  }
 }
 
 module.exports = Letter;
--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/test/Word.test.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/test/Word.test.js
@@ -1,38 +1,41 @@
-const Word = require('../lib/Word');
-const Letter = require('../lib/Letter');
+const Word = require("../lib/Word");
+const Letter = require("../lib/Letter");
 
-describe('Word class', () => {
-  it('Creates an array of Letter objects', () => {
-    const word = new Word('hi');
+describe("Word class", () => {
+  it("Creates an array of Letter objects", () => {
+    const word = new Word("hi");
 
     expect(word.letters).toEqual(
-      expect.arrayContaining([expect.objectContaining({ char: 'h' })])
+      expect.arrayContaining([expect.objectContaining({ char: "h" })])
     );
 
     expect(word.letters[0]).toBeInstanceOf(Letter);
   });
 
-  describe('guessLetter', () => {
-    it('Correct guess returns true', () => {
-      expect(new Word('fish').guessLetter('i')).toBe(true);
+  describe("guessLetter", () => {
+    it("Correct guess returns true", () => {
+      expect(new Word("fish").guessLetter("i")).toBe(true);
     });
 
-    it('Incorrect guess returns false', () => {
-      expect(new Word('fish').guessLetter('o')).toBe(false);
+    it("Incorrect guess returns false", () => {
+      expect(new Word("fish").guessLetter("o")).toBe(false);
     });
   });
 
-  describe('guessedCorrectly ', () => {
-    it('returns true if all letters are correct', () => {
-      const word = new Word('hi');
-      word.guessLetter('h');
-      word.guessLetter('i');
+  describe("guessedCorrectly ", () => {
+    it("returns true if all letters are correct", () => {
+      const word = new Word("hi");
+      console.log(word.letters);
+      word.guessLetter("h");
+      console.log(word.letters);
+      word.guessLetter("i");
+      console.log(word.letters);
       expect(word.guessedCorrectly()).toBe(true);
     });
-    it('returns false if at least one letter is incorrect', () => {
-      const word = new Word('hi');
-      word.guessLetter('h');
-      word.guessLetter('a');
+    it("returns false if at least one letter is incorrect", () => {
+      const word = new Word("hi");
+      word.guessLetter("h");
+      word.guessLetter("a");
       expect(word.guessedCorrectly()).toBe(false);
     });
   });
