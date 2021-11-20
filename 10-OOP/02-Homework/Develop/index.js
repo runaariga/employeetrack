fefe6e29fd1804605d5ff484fc11f8fe1e84ae01
Chadtao206 <chenchen408@gmail.com>
2021-11-20 14:11:47 -0800

    added starter code for homework 10

---
+++ 10-OOP/02-Homework/Develop/index.js
@@ -0,0 +1,8 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+//Write your app here
