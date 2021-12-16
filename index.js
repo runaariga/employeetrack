2c3d998e819b702972efcecb13407fc81cca6086
Runa Ariga <harunaariga@gmail.com>
2021-12-15 17:26:58 -0800

    changed parameters

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ const promptMessages = {
 const connection = mysql.createConnection({
     host: 'localhost',
 
-    // Your port; if not 3306
-    port: 3306,
+    port: 8080,
 
     // Your username
     user: 'root',
 
+
     // Your password
-    password: 'chargers619',
+    password: '123456',
     database: 'employees'
 });
 
